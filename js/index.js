7dbac36c3dc9c6f65dbb459c0f1257774eac58c3
Alexey Zharikov <mefis2.0@gmail.com>
2018-04-01 21:30:32 +0000

    update 1

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
+    function contClear() {
+        $(".text").empty();
+    }
     $(".cv").on('click', function() {
-        $("#text").addClass('cv');
-        $("#text").load('ajax/cv.html');
+        contClear();
+        $(".text").load('ajax/cv.html .cv_page');
+    })
+    $(".projects").on('click', function() {
+        contClear();
+        $(".text").load('ajax/cv.html .projects_page');
+    })
+    $(".main").on('click', function(){
+        return ('index.html');
     })
 });
